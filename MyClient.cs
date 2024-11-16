e2196485d814e345a9fc2859e8ad8b563bac57e4
konst-sh <k.v.sh@mail.ru>
2024-11-16 21:17:37 +0300

    Add project files.

---
+++ MyClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Netinteractive_Reft_issue;
+using Refit;
+
+public interface IMyRefitClient
+{
+    [Get("/openapi")]
+    Task<ApiResponse<string>> GetOpenapi();
+}
+
+public class ClientMaker
+{
+    public static IMyRefitClient MakeMyRefitClient() => RestService.For<IMyRefitClient>("https://cad.onshape.com/api/v9");
+}
