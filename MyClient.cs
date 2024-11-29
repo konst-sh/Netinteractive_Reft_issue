22cdbbe9c06f82105d04a214be21448f6d693c1b
konst-sh <k.v.sh@mail.ru>
2024-11-29 15:15:51 +0300

    Demoes how STJ9 fails to load in net interactive

--- MyClient.cs
+++ MyClient.cs
@@ -1,5 +1,6 @@
 ﻿namespace Netinteractive_Reft_issue;
 using Refit;
+using System.Text.Json;
 
 public interface IMyRefitClient
 {
@@ -9,5 +10,7 @@ public interface IMyRefitClient
 
 public class ClientMaker
 {
-    public static IMyRefitClient MakeMyRefitClient() => RestService.For<IMyRefitClient>("https://cad.onshape.com/api/v9");
+    private static JsonSerializerOptions opt = new() { AllowOutOfOrderMetadataProperties = true };
+    private static RefitSettings refitSettings = new() { ContentSerializer = new SystemTextJsonContentSerializer(opt) };
+    public static IMyRefitClient MakeMyRefitClient() => RestService.For<IMyRefitClient>("https://cad.onshape.com/api/v9", refitSettings);
 }
